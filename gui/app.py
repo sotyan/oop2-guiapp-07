a469ef1a7deaaa54501e641e9e0a98475c32cd02
Sota Tsukada <113979340+sotyan@users.noreply.github.com>
2025-11-26 21:20:36 +0900

    カメラ画像の撮影
    カメラの起動、画像撮影機能を実装しました。
    GUIボタンの上２つを押すと、それぞれが動作します。
    
    GUIウィンドウとカメラウィンドウの競合を防ぐために、GUI内でカメラ映像を表示しております。

--- gui/app.py
+++ gui/app.py
@@ -1,13 +1,18 @@
 from PySide6.QtWidgets import QApplication, QLabel, QMainWindow, QWidget, QVBoxLayout, QPushButton
-from PySide6.QtCore import Qt
+from PySide6.QtCore import Qt, QTimer
+from PySide6.QtGui import QImage, QPixmap
 import sys
+import threading
+import cv2
+
+from my_module.K24098.lecture05_camera_image_capture import MyVideoCapture
 
 class MainWindow(QMainWindow):
 	def __init__(self):
 		# メインウィンドウの作成
 		super().__init__()
 		self.setWindowTitle("背景加工アプリ")
-		self.setGeometry(100, 100, 300, 200)
+		self.setGeometry(100, 100, 640, 600)
 		central_widget = QWidget(self)
 		layout = QVBoxLayout(central_widget)
 
@@ -16,8 +21,13 @@ class MainWindow(QMainWindow):
 		label.setAlignment(Qt.AlignCenter)
 		layout.addWidget(label)
 
+		# カメラ画像表示用ラベル
+		self.image_label = QLabel(self)
+		self.image_label.setFixedSize(640, 480)
+		self.image_label.setAlignment(Qt.AlignCenter)
+		layout.addWidget(self.image_label)
+
 		# ボタン追加
-		# ボタンを押すと、画像撮影プログラムが呼び出される。（未実装）
 		self.btn_camera = QPushButton("カメラ起動", self)
 		self.btn_capture = QPushButton("画像撮影", self)
 		self.btn_show_processed = QPushButton("加工後画像表示", self)
